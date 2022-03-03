50a6633ec733389af9a1477abcbd38be8821860c
yt <asnedge.yt@gmail.com>
2022-03-03 10:49:40 +0900

    update environment

---
+++ frontend/app/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  moduleNameMapper: {
+    '\\.(css)$': '<rootDir>/node_modules/jest-css-modules',
+  },
+  roots: ['<rootDir>/src/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  testEnvironment: 'jest-environment-jsdom',
+}
